304dc199d2d68cb90833ff29b9055b7eff06e1ef
Daniel Mato <danny.mato1@gmail.com>
2020-06-02 13:44:55 -0400

    Removed old commented code from the login page

--- ComboCounter/Forms/Login.cs
+++ ComboCounter/Forms/Login.cs
@@ -12,49 +12,12 @@ namespace ComboCounter
         Main c = new Main();
         Register1 reg = new Register1();
 
-       /* private const String SERVER = "127.0.0.1";
-        private const String DATABASE = "project";
-        private const String UID = "root";
-        private const String PASSWORD = "692Trrbw83";
-        private static MySqlConnection dbConn;
-        private string username;
-        private string password;
-        public string fname;
-        public string lname;*/
-
 
         public Login()
         {
             InitializeComponent();
-            //InitializeDB();
         }
-        /*
-        public static void InitializeDB()
-        {
-            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
-            builder.Server = SERVER;
-            builder.UserID = UID;
-            builder.Password = PASSWORD;
-            builder.Database = DATABASE;
-            builder.SslMode = MySqlSslMode.None;
 
-            String connString = builder.ToString();
-
-            builder = null;
-
-            Console.WriteLine(connString);
-
-            dbConn = new MySqlConnection(connString);
-
-            Application.ApplicationExit += (sender, args) =>
-            {
-                if (dbConn != null)
-                {
-                    dbConn.Dispose();
-                    dbConn = null;
-                }
-            };
-        }*/
 
         // Reacts to button presses for the login button
         private void button1_Click(object sender, EventArgs e)
@@ -84,39 +47,6 @@ namespace ComboCounter
             Hide();
             reg.ShowDialog();
         }
-        /*
-        private bool GetLogin()
-        {
-            bool flag = false;
-            String query = "SELECT username, password, fname, lname FROM project.users WHERE username='" + username_tb.Text + "';";
-            String logQuery = "UPDATE project.users SET isloggedin=1 WHERE username='" + username_tb.Text + "';";
-
-            MySqlCommand cmd = new MySqlCommand(query, dbConn);
-            MySqlCommand cmd2 = new MySqlCommand(logQuery, dbConn);
-
-            dbConn.Open();
-
-            MySqlDataReader reader = cmd.ExecuteReader();
-
-            while (reader.Read())
-            {
-                username = (reader["username"]).ToString();
-                password = (reader["password"]).ToString();
-                fname = (reader["fname"]).ToString();
-                lname = (reader["lname"]).ToString();
-            }
-            reader.Close();
-
-            cmd2.ExecuteNonQuery();
-            dbConn.Close();
-
-            if (username != null && password_tb.Text == password)
-            {
-                flag = true;
-
-            }
-            return flag;
-        }*/
 
 
         // Makes a call to the database to verify the username and password typed by the user
