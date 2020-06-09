4e7563d0af1d06ba91008f1804e4a1281699ec9c
Daniel Mato <danny.mato1@gmail.com>
2020-06-09 17:16:37 -0400

    Moved the Theme classes to the classes folder

---
+++ ComboCounter/Classes/ThemeConstants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+public class ThemeConstants
+    {
+        public static Color white = Color.FromArgb(255, 255, 255);
+        public static Color green = Color.FromArgb(124, 252, 0);
+        public static Color red = Color.FromArgb(255, 0, 0);
+}
