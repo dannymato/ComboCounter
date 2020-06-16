fc5ea3a730f7d6c796ecc9790604f6082c0ac7cb
Daniel Mato <danny.mato1@gmail.com>
2020-06-16 13:06:25 -0400

    Calvin finished up the theme manager with the different colors we're gonna need

--- ComboCounter/Classes/ThemeManager.cs
+++ ComboCounter/Classes/ThemeManager.cs
@@ -17,22 +17,89 @@ namespace ComboCounter.Classes
 
         public static Color initBackground()
         {
-            // COMPARE WITH NEW
-            // Boolean ThemeBoolean = Verify();
+           
             if (Verify())
             {
-                // DEFINE METOD FOR RETURNING ISHHH
                 return ChangeBackground(GetDBThemeNum());
-
             }
             else
             {
                 return ChangeBackground(GetThemeNum());
             }
-            
-            //IF FALSE APPLY NEW THEME
-        
+      
         }
+
+        public static Color initButtonColor()
+        {
+            if (Verify())
+            {
+                return ChangeButtonColor(GetDBThemeNum());
+
+            }
+            else
+            {
+                return ChangeButtonColor(GetThemeNum());
+            }
+        }
+
+        public static Color initHeaderColor()
+        {
+            if (Verify())
+            {
+                // DEFINE METOD FOR RETURNING ISHHH
+                return ChangeHeaderColor(GetDBThemeNum());
+
+            }
+            else
+            {
+                return ChangeHeaderColor(GetThemeNum());
+            }
+        }
+
+
+        public static Color initH2Color()
+        {
+            if (Verify())
+            {
+            
+                return ChangeH2Color(GetDBThemeNum());
+
+            }
+            else
+            {
+                return ChangeH2Color(GetThemeNum());
+            }
+        }
+
+        public static Color initH3Color()
+        {
+            if (Verify())
+            {
+                return ChangeH3Color(GetDBThemeNum());
+
+            }
+            else
+            {
+                return ChangeH3Color(GetThemeNum());
+            }
+        }
+
+        public static Color initTextColor()
+        {
+            if (Verify())
+            {
+                return ChangeTextColor(GetDBThemeNum());
+
+            }
+            else
+            {
+                return ChangeTextColor(GetThemeNum());
+            }
+        }
+
+
+
+        //----------------------------
         public static Boolean Verify()
         {
             // GET LAST THEME NUMBER
@@ -57,8 +124,12 @@ namespace ComboCounter.Classes
         {
             return DBThemeNum;
         }
-    
 
+        //----------------------------
+
+
+        // CHANGE COLOR ELEMENTS
+        
         private static Color ChangeBackground(int t)
         {
          
@@ -82,10 +153,113 @@ namespace ComboCounter.Classes
         }
 
 
+        private static Color ChangeButtonColor(int t)
+        {
 
 
+            if (t == 1)
+            {
+                return ThemeConstants.white;
+            }
+            else if (t == 2)
+            {
+                return ThemeConstants.red;
+            }
+            else if (t == 3)
+            {
+                return ThemeConstants.green;
+            }
+            else
+            {
+                return ThemeConstants.white;
+            }
+        }
 
-       
+
+        private static Color ChangeHeaderColor(int t)
+        {
+
+
+            if (t == 1)
+            {
+                return ThemeConstants.white;
+            }
+            else if (t == 2)
+            {
+                return ThemeConstants.red;
+            }
+            else if (t == 3)
+            {
+                return ThemeConstants.green;
+            }
+            else
+            {
+                return ThemeConstants.white;
+            }
+        }
+
+        private static Color ChangeH2Color(int t)
+        {
+
+            if (t == 1)
+            {
+                return ThemeConstants.white;
+            }
+            else if (t == 2)
+            {
+                return ThemeConstants.red;
+            }
+            else if (t == 3)
+            {
+                return ThemeConstants.green;
+            }
+            else
+            {
+                return ThemeConstants.white;
+            }
+        }
+
+        private static Color ChangeH3Color(int t)
+        {
+            if (t == 1)
+            {
+                return ThemeConstants.white;
+            }
+            else if (t == 2)
+            {
+                return ThemeConstants.red;
+            }
+            else if (t == 3)
+            {
+                return ThemeConstants.green;
+            }
+            else
+            {
+                return ThemeConstants.white;
+            }
+        }
+
+        private static Color ChangeTextColor(int t)
+        {
+            if (t == 1)
+            {
+                return ThemeConstants.white;
+            }
+            else if (t == 2)
+            {
+                return ThemeConstants.red;
+            }
+            else if (t == 3)
+            {
+                return ThemeConstants.green;
+            }
+            else
+            {
+                return ThemeConstants.white;
+            }
+        }
+
+
 
     }
 }
